c24264f9fb727078a8bfdb872698b13e65969640
saketh404 <saketh404@gmail.com>
2024-10-10 00:40:45 +0530

    third commit

--- src/context/NotesContextProvider.jsx
+++ src/context/NotesContextProvider.jsx
@@ -38,25 +38,3 @@ export default function NotesContextProvider({ children }) {
     </NotesContext.Provider>
   );
 }
-
-// sample data repersentation
-
-// const sampledata = [
-//   {
-//     title: "sample",
-//     color: "hsla(262, 92%, 76%, 1)",
-//     epoch: 1702147109,
-//     notes: [
-//       { body: "this is 1st sample note", epoch: 1702147145 },
-//       { body: "this is 2nd sample note", epoch: 1702147155 },
-//     ],
-//   },{
-//     title: "sample 2",
-//     color: "hsla(262, 92%, 76%, 1)",
-//     epoch: 1702147106,
-//     notes: [
-//       { body: "this is 1st sample note", epoch: 1702147175 },
-//       { body: "this is 2nd sample note", epoch: 1702147185 },
-//     ],
-//   }
-// ];
